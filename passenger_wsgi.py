4d85d7e5eaf67fb91ec25d645d445da70bf7ec37
Daguilar0123 <aguilard107@gmail.com>
2025-05-14 17:43:39 -0400

    Clean up comments and simplify sys.path configuration in passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,5 @@
 import sys, os
-
-# 1) Make our vendored lib/ directory importable
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
-
-# 2) Add the app root so "import app" works
+# make sure this directory is on sys.path
 sys.path.insert(0, os.path.dirname(__file__))
 
 from app import app as application
